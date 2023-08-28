d92f16d27e75c538a3b80a1cae99e48e13bb3df3
RockFall <caiohdrocha@gmail.com>
2023-08-28 08:31:12 -0300

    Preparing to codemod Next images

--- src/components/BannerCarousel/components/OutfitBanner/OutfitBanner.tsx
+++ src/components/BannerCarousel/components/OutfitBanner/OutfitBanner.tsx
@@ -1,12 +1,106 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { makeStyles } from '@mui/styles'
+import { styled } from '@mui/material/styles'
 import { Link, Hidden } from '@mui/material'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import Image from 'next/image'
 
 //import { HighlightBanner } from '../../../../generated/graphql'
 import { HighlightBanner } from '../../../../util/custom_types'
+
+const DivRoot = styled("div")(({ theme }) => ({
+  boxShadow: "none",
+}));
+
+const StyledLink = styled(Link)(({ theme }) => ({
+  fontFamily: "Suisse Int\\'l",
+  fontStyle: "normal",
+  fontWeight: 600,
+  fontSize: "1rem",
+  lineHeight: "32px",
+  textAlign: "right",
+  padding: "10px 0px",
+}));
+
+const LinkMobileLink = styled(Link)(({ theme }) => ({
+  fontFamily: "Suisse Int\\'l",
+  fontStyle: "normal",
+  fontWeight: 600,
+  fontSize: "20px",
+  lineHeight: "32px",
+}));
+
+const DivMobileSlide = styled("div")(({ theme }) => ({
+  position: "absolute",
+  bottom: "50px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const DivSlide = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: "25%",
+  left: "60%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const DivCroqui = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: "2%",
+  left: "30%",
+  height: "88%",
+  width: "25%",
+}));
+
+const DivMobileCroqui = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: "0.5%",
+  left: "17.5%",
+  width: "65%",
+  height: "80%",
+}));
+
+const ArrowForwardIosIconArrowIcon = styled(ArrowForwardIosIcon)(
+  ({ theme }) => ({
+    color: "#6F6C6B",
+    fontSize: theme.spacing(1.2),
+    marginLeft: "5px",
+  })
+);
+
+const DivMobileImages = styled("div")(({ theme }) => ({
+  paddingTop: "86.60%",
+  width: "100%",
+  maxHeight: "478px",
+  height: "80%",
+}));
+
+const DivMobileCentering = styled("div")(({ theme }) => ({
+  width: "100%",
+  maxHeight: "100%",
+  height: "130vw",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const DivMobileBackgroundPosition = styled("div")(({ theme }) => ({
+  position: "absolute",
+  top: "0",
+  width: "100%",
+  height: "auto",
+  minHeight: "100%",
+  minWidth: "100%",
+}));
+
+
 const useStyles = makeStyles(theme => ({
   root: {
     boxShadow: 'none',
@@ -97,44 +191,68 @@ const OutfiBanner = (props: OutfiBannerProps) => {
   const { item, className, ...rest } = props
   const classes = useStyles()
 
-  
 
-// TODO: Change Croqui Image back to Webg
+
+  // TODO: Change Croqui Image back to Webg
   return (
-    <div className={classes.root}>
+    <DivRoot>
       <Hidden smDown>
-        <Image src={`/${item.background}`} width={1920} height={934} layout='responsive' objectFit='cover' alt="Croqui Background" />
-        <div className={classes.croqui}>
-          <Image src={item.images[0].image.original} layout='fill' objectFit='contain' alt="Croqui image" />
-        </div>
-        <div className={classes.slide}>
+        <Image
+          src={`/${item.background}`}
+          width={1920}
+          height={934}
+          layout="responsive"
+          objectFit="cover"
+          alt="Croqui Background"
+        />
+        <DivCroqui>
+          <Image
+            src={item.images[0].image.original}
+            layout="fill"
+            objectFit="contain"
+            alt="Croqui image"
+          />
+        </DivCroqui>
+        <DivSlide>
           {item.links.map((link, i) => (
-            <Link href={link.url} className={classes.link} key={i}>
-              {link.text} <ArrowForwardIosIcon className={classes.arrowIcon}></ArrowForwardIosIcon>{' '}
-            </Link>
+            <StyledLink href={link.url} key={i}>
+              {link.text}{" "}
+              <ArrowForwardIosIconArrowIcon></ArrowForwardIosIconArrowIcon>{" "}
+            </StyledLink>
           ))}
-        </div>
+        </DivSlide>
       </Hidden>
       <Hidden smUp>
-        <div className={classes.mobileCentering}>
-          <div className={classes.mobileImages}>
-            <div className={classes.mobileBackgroundPosition}>
-              <Image src={`/${item.background}`} layout='fill' objectFit='contain' alt=""  />
-            </div>
-            <div className={classes.mobileCroqui}>
-              <Image src={item.images[0].image.original} layout='fill' objectFit='contain' alt=""  />
-            </div>
-          </div>
-          <div className={classes.mobileSlide}>
+        <DivMobileCentering>
+          <DivMobileImages>
+            <DivMobileBackgroundPosition>
+              <Image
+                src={`/${item.background}`}
+                layout="fill"
+                objectFit="contain"
+                alt=""
+              />
+            </DivMobileBackgroundPosition>
+            <DivMobileCroqui>
+              <Image
+                src={item.images[0].image.original}
+                layout="fill"
+                objectFit="contain"
+                alt=""
+              />
+            </DivMobileCroqui>
+          </DivMobileImages>
+          <DivMobileSlide>
             {item.links.map((link, _i) => (
-              <Link key={_i} href={link.url} className={classes.mobileLink}>
-                {link.text} <ArrowForwardIosIcon className={classes.arrowIcon}></ArrowForwardIosIcon>{' '}
-              </Link>
+              <LinkMobileLink key={_i} href={link.url}>
+                {link.text}{" "}
+                <ArrowForwardIosIconArrowIcon></ArrowForwardIosIconArrowIcon>{" "}
+              </LinkMobileLink>
             ))}
-          </div>
-        </div>
+          </DivMobileSlide>
+        </DivMobileCentering>
       </Hidden>
-    </div>
+    </DivRoot>
   );
 }
 
